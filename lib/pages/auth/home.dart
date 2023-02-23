6e283a1c6842d55802b3057d7d90660ae63f0066
Nico <nicoffb@hotmail.com>
2023-02-23 11:09:34 +0100

    intento de nav

---
+++ lib/pages/auth/home.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc_authentication/pages/auth/auth_screen.dart';
+import 'package:get/get.dart';
+import 'package:get/get_core/src/get_main.dart';
+
+import 'package:shared_preferences/shared_preferences.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  final Future<SharedPreferences> _prefs = SharedPreferences.getInstance();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(actions: [
+        TextButton(
+            onPressed: () async {
+              final SharedPreferences? prefs = await _prefs;
+              prefs?.clear();
+              Get.offAll(AuthScreen());
+            },
+            child: Text(
+              'logout',
+              style: TextStyle(color: Colors.white),
+            ))
+      ]),
+      body: Center(
+        child: Column(
+          children: [
+            Text('Welcome home'),
+            TextButton(
+                onPressed: () async {
+                  final SharedPreferences? prefs = await _prefs;
+                  print(prefs?.get('token'));
+                },
+                child: Text('print token'))
+          ],
+        ),
+      ),
+    );
+  }
+}
